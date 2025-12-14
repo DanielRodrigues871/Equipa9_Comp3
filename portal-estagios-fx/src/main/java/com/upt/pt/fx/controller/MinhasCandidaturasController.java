4339791ad28bac98a38c0ad2878166b6d04f95eb
momou <momou@10.179.13.56>
2025-12-14 19:51:42 +0000

    .

--- portal-estagios-fx/src/main/java/com/upt/pt/fx/controller/MinhasCandidaturasController.java
+++ portal-estagios-fx/src/main/java/com/upt/pt/fx/controller/MinhasCandidaturasController.java
@@ -13,7 +13,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
-//Candidaturas que um estudante fez
+//Candidaturas que um estudante fezz
 public class MinhasCandidaturasController {
 
     @FXML private TableView<CandidaturaFX> tabela;

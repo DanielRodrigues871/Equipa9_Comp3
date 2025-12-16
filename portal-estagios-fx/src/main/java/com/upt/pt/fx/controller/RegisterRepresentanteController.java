3a28ae19a23754b5ab4506fe08346942119211e3
momou <momou@10.179.13.56>
2025-12-16 17:52:58 +0000

    no registar representante ja da para criar empresa caso ela ainda n existar e aparecem os ids das empresas ja criadas para nao se ter de ir ha base de dados

--- portal-estagios-fx/src/main/java/com/upt/pt/fx/controller/RegisterRepresentanteController.java
+++ portal-estagios-fx/src/main/java/com/upt/pt/fx/controller/RegisterRepresentanteController.java
@@ -4,7 +4,10 @@ import com.upt.pt.SceneManager;
 import com.upt.pt.fx.service.ApiClient;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import com.upt.pt.fx.model.EmpresaOption;
+
 
 public class RegisterRepresentanteController {
 
@@ -12,22 +15,52 @@ public class RegisterRepresentanteController {
     @FXML private TextField emailField;
     @FXML private PasswordField passwordField;
 
-    @FXML private TextField empresaIdField;
+    @FXML private ComboBox<EmpresaOption> empresaCombo;
     @FXML private TextField cargoField;
     @FXML private TextField telefoneField;
 
     @FXML private Label errorLabel;
 
     @FXML
+    public void initialize() {
+        carregarEmpresas();
+    }
+
+    private void carregarEmpresas() {
+        try {
+            JSONArray arr = ApiClient.getArray("/api/empresas");
+            empresaCombo.getItems().clear();
+
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject e = arr.getJSONObject(i);
+                empresaCombo.getItems().add(
+                        new EmpresaOption(
+                                e.getString("id"),
+                                e.getString("nome")
+                        )
+                );
+            }
+        } catch (Exception e) {
+            errorLabel.setText("Erro ao carregar empresas.");
+        }
+    }
+
+    @FXML
     public void registar() {
         try {
+            EmpresaOption empresa = empresaCombo.getValue();
+            if (empresa == null) {
+                errorLabel.setText("Selecione uma empresa.");
+                return;
+            }
+
             JSONObject json = new JSONObject();
             json.put("nome", nomeField.getText());
             json.put("email", emailField.getText());
             json.put("password", passwordField.getText());
             json.put("tipo", "REPRESENTANTE");
 
-            json.put("empresaId", empresaIdField.getText());
+            json.put("empresaId", empresa.getId());
             json.put("cargo", cargoField.getText());
             json.put("telefone", telefoneField.getText());
 
@@ -38,6 +71,11 @@ public class RegisterRepresentanteController {
         } catch (Exception e) {
             errorLabel.setText("Erro no registo do representante!");
         }
+    }
+
+    @FXML
+    public void criarEmpresa() {
+        SceneManager.changeScene("criar_empresa.fxml");
     }
 
     @FXML
